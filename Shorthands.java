9ed688e26d13b4d1d52b68c6edb15fff1f189890
Aishwarya M <48712269+aish-muralidhar@users.noreply.github.com>
2024-07-25 22:16:14 +0530

    Add files via upload

---
+++ Shorthands.java
@@ -0,0 +1,21 @@
+class Shorthands
+{
+        public static void main(String args[])
+        {
+                int a=10;
+                a+=2;
+                System.out.println("Result after applying a+=2:"+a);
+                a-=2;
+                System.out.println("Result after applying a-=2:"+a);
+                a*=2;
+                System.out.println("Result after applying a*=2:"+a);
+                a/=2;
+                System.out.println("Result after applying a/=2:"+a);
+                a%=2;
+                System.out.println("Result after applying a%=2:"+a);
+                //Increment
+                a++;
+                System.out.println("Result after applying a++:"+a);
+                
+        }
+}
