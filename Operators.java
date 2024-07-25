541566c30ae40f017e9f412f2ff80879f6a6d359
Aishwarya M <48712269+aish-muralidhar@users.noreply.github.com>
2024-07-25 22:09:01 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,19 @@
+class Operators
+{   
+    public static void main(String args[])
+    {
+        int a=5,b=20;
+        int c;
+        c=a+b;
+        System.out.println("Result after addition:"+c);
+        c=a-b;
+        System.out.println("Result after subtraction:"+c);
+        c=a*b;
+        System.out.println("Result after multiplication:"+c);
+        c=b/a;
+        System.out.println("Result after divison:"+c);
+        c=b%a;
+        System.out.println("Result after using modulus operator:"+c);
+    }
+}
+        
