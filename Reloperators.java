0aa4d85b7a400495cd4d6a7a38d714a9f0855041
Aishwarya M <48712269+aish-muralidhar@users.noreply.github.com>
2024-08-08 13:10:44 +0530

    Add files via upload

---
+++ Reloperators.java
@@ -0,0 +1,20 @@
+class Reloperators
+{
+    public static void main(String args[])
+    {
+        int x=5,y=6;
+        boolean r=x<y;
+        System.out.println(r);
+        boolean s=x>y;
+        System.out.println(s);
+        //x=7;
+        boolean t=x<=y;
+        System.out.println(t);
+        boolean u=x>=y;
+        System.out.println(u);
+        boolean v=x!=y;
+        System.out.println(v);
+        boolean w=x==y;
+        System.out.println(w);
+    }
+}
